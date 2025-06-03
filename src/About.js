c3f3909816774d521b3c34252e17bd6857583dd6
Sathvik Nookala <sathviknookala@Mac.fios-router.home>
2025-06-02 21:34:59 -0400

    Changed About page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>About Page</h1>
-      <p>About Page Coming Soon!</p>
+    <div className="about-container" style={{ height: '100vh' }}>
+      <h1 className="heading-primary text-center">Meet the Team</h1>
+      <div className="content-section">
+        <p className="paragraph">
+          Project Hoops started with an idea to create 
+          a platform for basketball enthusiasts to connect and 
+          share their love for the sport. Our team is dedicated 
+          to providing a space where players can find opportunities to play,
+          improve, and grow.
+        </p>
+      </div>
     </div>
   );
 };
